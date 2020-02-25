330021f5ffa23f33d4310ca4d602712383957747
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-02-25 18:34:22 +0100

    up

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+    <title>AJAX DISCHI</title>
+  </head>
+  <body>
+    <header>
+      <div id="navbar">
+        <img src="img/logo.png" alt="logo">
+      </div>
+    </header>
+    <main>
+
+    </main>
+  </body>
+</html>
